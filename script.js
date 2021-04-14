3740b3095b1398153a4fb5f275ed830678ff70e4
Mishaproh96 <mishaprohorenko96@gmail.com>
2021-04-14 20:01:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 'use strict'
 
+const sum = (a, b) => a + b;
+const getRandomNumber = () => {
+    Math.floor(Math.random() * 10)
+}
+
+const logerDecorator = function (func, userName, funcName) {
+    let counter = 0;
+    return function () {
+        counter++
+        let result = func.call(null, ...arguments)
+        let userData = `${userName} called ${funcName} ${counter} times whith ${result}`
+        globalLog.push(userData)
+        return result
+    }
+};
+
+const sumForNick = logerDecorator(sum, 'Nick', 'sum')
+const sumForMike = logerDecorator(sum, 'Mike', 'sum')
+const getRandomForNick = logerDecorator(getRandomNumber, 'Nick', 'random')
+const getRandomForMike = logerDecorator(getRandomNumber, 'Mike', 'random')
+
+let globalLog = [];
+
+
+sumForMike(1, 2)
+sumForMike(10, 30)
+sumForMike(10, 30)
+
+getRandomForNick()
+
+getRandomForMike()
+sumForNick(2, 7)
+sumForNick(9, 34)
+sumForNick(9, 34)
+sumForNick(9, 34)
+
+getRandomForNick()
+
+getRandomForMike()
+
+console.log(globalLog)
